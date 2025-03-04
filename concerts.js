fc11e1c15e8a2af8368deb4e9c3afad285bd624f
Noiceinmydrink <eesangeree@gmail.com>
2025-03-04 10:31:06 +0545

    Added contents to the concerts page

--- concerts.js
+++ concerts.js
@@ -1,3 +1,5 @@
+import {tickets} from './tickets.js';
+
 function toggleSidebar(){
 
     //All the classes of the needed buttons and divs are fetched here and stored in a variable.
@@ -22,3 +24,48 @@ function toggleSidebar(){
 //Then the function is being deployed here.
 document.querySelector('.js-open-btn').addEventListener('click', toggleSidebar);
 document.querySelector('.js-close-btn').addEventListener('click', toggleSidebar);
+
+let concertsHTML = '';
+
+tickets.forEach((shows)=>{
+ concertsHTML += `<div class="show-info-container">
+          <div class="show-image-container">
+            <img class="show-image"
+              src="${shows.image}">
+          </div>
+
+          <div class="show-name limit-text-to-2-lines">
+            ${shows.name}
+          </div>
+
+          <div class="show-price">
+            Rs.${(shows.priceRupees)}
+          </div>
+
+          <div class="show-quantity-container">
+            <select style = "height: 35px; width: 38px; border-radius: 3px;">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="show-spacer"></div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-show-id = "${shows.id}"> 
+            Add to Cart
+          </button>
+        </div>`;   
+});
+
+console.log(concertsHTML);
+
+document.querySelector('.js-main-content').innerHTML = concertsHTML;
---
+++ tickets.js
@@ -0,0 +1,20 @@
+export let tickets = [{
+
+    id: 1,
+    image: 'images/Albatross.png',
+    name: 'Albatross concert',
+    priceRupees: 1500
+
+},
+{   id: 2,
+    image: 'images/sabinrai.png',
+    name: 'Sabin Rai concert',
+    priceRupees: 1800
+},
+{
+    id: 3,
+    image: 'images/Nepathya.png',
+    name: 'Nepathya concert',
+    priceRupees: 2500
+}
+]
